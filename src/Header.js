a171e568bf9a98d9b54672b71e9ae3ce68cd2098
Igune Justine Robert <igunejustinerobert@gmail.com>
2020-10-09 03:15:21 +0300

    Modified file

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '.Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
   return (
@@ -31,6 +32,15 @@ function Header() {
         <span className='header__optionLineOne'>Your</span>
         <span className='header__optionLineTwo'>Prime</span>
       </div>
+
+      <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
       
       </div> 
 
